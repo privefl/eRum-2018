1ce6efe2fcd9f92b9c1b3090cad652f4ce77d182
privefl <florian.prive.21@gmail.com>
2018-04-25 11:39:01 +0200

    need more changes

---
+++ big_colsums.cpp
@@ -0,0 +1,26 @@
+// [[Rcpp::depends(BH, bigstatsr)]]
+#include <bigstatsr/BMCodeAcc.h>
+
+// [[Rcpp::export]]
+NumericVector big_colsums(Environment BM) {
+  
+  XPtr<FBM> xpBM = BM["address"]; 
+  BMAcc<double> macc(xpBM); 
+  
+  size_t n = macc.nrow();
+  size_t m = macc.ncol();
+  
+  NumericVector res(m);
+  
+  for (size_t j = 0; j < m; j++) 
+    for (size_t i = 0; i < n; i++)
+      res[j] += macc(i, j);
+  
+  return res;
+}
+
+/*** R
+library(bigstatsr)
+tmp <- FBM(10, 10, init = 1:100)
+big_colsums(tmp)
+*/
