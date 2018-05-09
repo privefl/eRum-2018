7e10c0bb73698f0eec9eb0acef464f6bf521f417
Florian Privé <florian.prive.21@gmail.com>
2018-05-09 15:27:46 +0200

    autoplay

--- HTML2PDF.R
+++ HTML2PDF.R
@@ -2,6 +2,6 @@ system(glue::glue(
   "decktape-1.0.0/phantomjs",
   " decktape-1.0.0/decktape.js", 
   " slides.html slides.pdf", 
-  " --load-pause 1000", 
-  " -s 1504x1129"
+  " --load-pause 2000", 
+  " -s 1505x1129"
 ))
